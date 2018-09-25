3ca341c9eadede82d939af0bb22e6c2bff32eb19
CarterJ7705 <43561225+CarterJ7705@users.noreply.github.com>
2018-09-24 23:24:17 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_JoshCarter.py
@@ -0,0 +1,15 @@
+#Get the projected total sales.
+#9/24/18
+#CTI-110 P2Y1 - Sales Prediction
+#Josh Carter
+
+total_sales= float(input('Enter the projected sales: "))
+                   
+#Calculate the profit as 23 percent of total sales.
+   profit = total_sales * 0.23
+                         
+#Display the profit.
+print("The profit is $, format(profit, ",.2f"))
+
+
+
